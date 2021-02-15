dec498fe82ff3d3ca3e110a96296fd045119466f
lexasby <alexey.aka.lexas@gmail.com>
2021-02-15 15:39:17 +0300

    feat: add division into skill levels

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Whack-A-Mole
+Welcome to my Whack-a-Molle Game:
+https://lexasby.github.io/Whack-A-Mole/
--- script.js
+++ script.js
@@ -2,10 +2,16 @@ const holes = document.querySelectorAll('.hole');
 const scoreBoard = document.querySelector('.score');
 const moles = document.querySelectorAll('.mole');
 const lastScore = document.querySelector('.lastScore');
+const currentLevel = document.querySelector('.currentLevel');
 let lastHole;
 let timeUp = false;
 let score = 0;
-lastScore.textContent = localStorage.getItem('lastScore');
+currentLevel.textContent = localStorage.getItem('currentLevel') || 'unknown yet';
+lastScore.textContent = localStorage.getItem('lastScore') || 0;
+
+let level;
+let minTime;
+let maxTime;
 
 function randomTime(min, max) {
   return Math.round(Math.random() * (max - min) + min);
@@ -15,15 +21,42 @@ function randomHole(holes) {
   const idx = Math.floor(Math.random() * holes.length);
   const hole = holes[idx];
   if (hole === lastHole) {
-    console.log('Ah nah thats the same one bud');
+    //console.log('Ah nah thats the same one bud');
     return randomHole(holes);
   }
   lastHole = hole;
   return hole;
 }
-
+// function checkLevel () {
+  
+// }
 function peep() {
-  const time = randomTime(200, 1000);
+  let level = localStorage.getItem('lastScore')
+  
+  if (level <= 6) {
+    localStorage.setItem('currentLevel', 'are newbie')
+    minTime = 400;
+    maxTime = 2000;
+    console.log('easy')
+    
+  }
+  if (level > 6 && level <= 10) {
+    localStorage.setItem('currentLevel', 'are medium')
+    minTime = 200;
+    maxTime = 1000;
+    console.log('medium')
+    
+  }
+  if (level > 10) {
+    localStorage.setItem('currentLevel', 'are professional')
+    minTime = 100;
+    maxTime = 800;
+    console.log('hard')
+    
+  }
+  
+  const time = randomTime (minTime, maxTime);
+ 
   const hole = randomHole(holes);
   hole.classList.add('up');
   setTimeout(() => {
@@ -36,8 +69,10 @@ function startGame() {
   scoreBoard.textContent = 0;
   timeUp = false;
   score = 0;
+  lastScore.textContent = localStorage.getItem('lastScore') || 0;
+  currentLevel.textContent = localStorage.getItem('currentLevel') || 'unknown yet';
   peep();
-  setTimeout(() => timeUp = true, 10000)
+  setTimeout(() => timeUp = true, 15000)
 }
 
 function startEasyGame() {
@@ -54,6 +89,7 @@ function bonk(e) {
   this.parentNode.classList.remove('up');
   scoreBoard.textContent = score;
   localStorage.setItem('lastScore', score)
+
 }
 
 
