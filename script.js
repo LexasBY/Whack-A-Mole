36f86b778d4b1c3ffae7ae8564776cdc65aad0be
lexasby <alexey.aka.lexas@gmail.com>
2021-02-16 10:42:56 +0300

    feat: fix bugs and add some comments

--- script.js
+++ script.js
@@ -13,46 +13,43 @@ let level;
 let minTime;
 let maxTime;
 
-function randomTime(min, max) {
-  return Math.round(Math.random() * (max - min) + min);
+//Getting a random number between two values:
+function randomTime(min, max) { 
+  return Math.round(Math.random() * (max - min) + min);  // Math.round()  returns the value of a number rounded to the nearest integer(Math.random() returns a random number between 0 (inclusive),  and 1 (exclusive):) 
 }
 
+//Getting a Hole wit a random index 
 function randomHole(holes) {
-  const idx = Math.floor(Math.random() * holes.length);
+  const idx = Math.floor(Math.random() * holes.length); //Math.floor() function returns the largest integer less than or equal to a given number.
   const hole = holes[idx];
-  if (hole === lastHole) {
-    //console.log('Ah nah thats the same one bud');
+  if (hole === lastHole) {  
     return randomHole(holes);
   }
   lastHole = hole;
   return hole;
 }
-// function checkLevel () {
-  
-// }
+
 function peep() {
   let level = localStorage.getItem('lastScore')
   
   if (level <= 6) {
-    localStorage.setItem('currentLevel', 'are newbie')
+    localStorage.setItem('currentLevel', 'is newbie')
     minTime = 400;
     maxTime = 2000;
     console.log('easy')
     
   }
   if (level > 6 && level <= 10) {
-    localStorage.setItem('currentLevel', 'are medium')
+    localStorage.setItem('currentLevel', 'is medium')
     minTime = 200;
     maxTime = 1000;
     console.log('medium')
-    
   }
   if (level > 10) {
-    localStorage.setItem('currentLevel', 'are professional')
+    localStorage.setItem('currentLevel', 'is professional')
     minTime = 100;
     maxTime = 800;
     console.log('hard')
-    
   }
   
   const time = randomTime (minTime, maxTime);
@@ -70,7 +67,7 @@ function startGame() {
   timeUp = false;
   score = 0;
   lastScore.textContent = localStorage.getItem('lastScore') || 0;
-  currentLevel.textContent = localStorage.getItem('currentLevel') || 'unknown yet';
+  currentLevel.textContent = localStorage.getItem('currentLevel') || 'is not known yet';
   peep();
   setTimeout(() => timeUp = true, 15000)
 }
